1633a83a96184f589e8ad042b6dc6e0406e53da3
Denis <denis.denisenko@gmail.com>
2021-02-01 17:53:12 +0200

    added skrzaat

--- next bigger number.py
+++ next bigger number.py
@@ -1,11 +1,14 @@
 def next_bigger(n):
     s = list(str(n))
-    i = j = len(s) - 1
-    while i > 0 and s[i - 1] <= s[i]: i -= 1
-    if i <= 0: return -1
-    while s[j] >= s[i - 1]: j -= 1
-    s[i - 1], s[j] = s[j], s[i - 1]
-    s[i:] = reversed(s[i:])
-    if s[0] == '0': return -1
+    i = j = 0
+    length = len(s)-1
+    while i < length and s[i + 1] <= s[i]: i -= 1
+    if i >= length: return -1
+    while s[j] >= s[i + 1]: j += 1
+    s[i], s[j+1] = s[j+1], s[i]
+    s[:i] = reversed(s[:i])
+    if s[length] == '0': return -1
     print(int(''.join(s)))
     return int(''.join(s))
+
+next_bigger(12)
