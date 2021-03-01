d692b8969982f983fb1eee729cd6b05d625eae7c
Denis <denis.denisenko@gmail.com>
2021-03-01 20:49:49 +0200

    count ones

---
+++ longest_substring_k_my_example.py
@@ -0,0 +1,13 @@
+def kUniques(s, k):
+    found = 0
+    leng = 0
+    same = 0
+    for i in range(0, len(s) - 1):
+        for j in range(i, len(s) - 1):
+            if (s[i] == s[i + 1] and same <= k):
+                print("D")
+
+
+s = "aabacbebebe"
+k = 3
+kUniques(s, k)
